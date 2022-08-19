2e536c58b74360ca0600aec0c9166a4bdda006a0
Brooks <brooks@karlik.org>
2022-08-19 01:22:19 -0700

    symlink in compile instructions

--- build.sh
+++ build.sh
@@ -19,3 +19,7 @@ git checkout v1.0.2 && \
 ./mconfig --prefix=/opt/apptainer && \
     make -C ./builddir && \
     sudo make -C ./builddir install
+
+# add apptainer to /usr/local/bin w/ symlink
+# this is probably devastatingly bad if it was installed with a package manager
+sudo ln -s /opt/apptainer/bin/apptainer /usr/local/bin/apptainer
